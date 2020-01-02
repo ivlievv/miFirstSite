e144077e494fc9bcf5567638a98adb65c988a37a
Ihor Ivliev <igor852745@gmail.com>
2020-01-02 16:35:07 +0200

    my test commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import './assets/css/reset.css';
 import './assets/scss/layout.scss';
 import './assets/scss/styles.scss';
 
+fetch('./data/employees.json')
+    .then(response => response.json())
+    .then(console.log)
+    .catch(console.error);
 

6c14bc940232ee9df6e2cad70b8388ab2e9c5d14
Ihor Ivliev <igor852745@gmail.com>
2020-01-03 18:08:45 +0200

    animation first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './assets/css/reset.css';
 import './assets/scss/layout.scss';
 import './assets/scss/styles.scss';
 import './assets/scss/content.scss';
+import './assets/scss/imagesAnimation.scss';
 
 fetch('./data/employees.json')
     .then(response => response.json())

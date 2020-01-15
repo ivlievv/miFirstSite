c8b6044efd99089fb29c74980d2a785d54df4a42
Ihor Ivliev <igor852745@gmail.com>
2020-01-15 10:09:55 +0200

    correction html commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './assets/css/reset.css';
 import './assets/scss/layout.scss';
 import './assets/scss/styles.scss';
 import './assets/scss/content.scss';
-import './assets/scss/imagesAnimation.scss';
+import './assets/scss/works.scss';
 import './assets/scss/navButton.scss';
 import './assets/scss/navigation.scss';
 import './assets/scss/team.scss';
